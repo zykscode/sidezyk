d831769de1b6e214c511981cac562524f80a4010
zykcode <zyskson@gmail.com>
2024-08-19 06:06:10 +0100

    feat: initial commit

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,4 @@
-import { SiteConfig } from '@/types';
-
-export const siteConfig: SiteConfig = {
+export const siteConfig = {
   name: 'Zykson',
   description: 'A personal blog and portfolio site',
   url: 'https://tx.shadcn.com',
